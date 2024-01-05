33b58d2216a0d4cc05aa0fec19012e53ff3460e4
Robopol <88227243+robopol@users.noreply.github.com>
2024-01-05 05:18:31 +0100

    YouTube skipper
    # YouTube Skipper Script
    
    An automated script for skipping ads on YouTube when using full-screen mode.
    
    ## Features
    
    - Easy initiation with keyboard shortcuts (F6 to start, F8 to stop).
    - Supports screen resolutions 3840 x 2160 and 2560 x 1440.
    
    ## How to Use
    
    1. Press F6 to start the script.
    2. The script automatically detects the screen resolution and sets the skip ad button position.
    3. Ads will be automatically skipped in full-screen mode.
    
    ## Note
    
    Before usage, make sure you have the necessary libraries installed (pyautogui, pynput, keyboard, Pillow).
    
    **Note**: This script may work only for specific screen resolutions. If you have a different resolution, you can modify the script according to your needs.
    
    Thank you for using!
    
    [robopol.sk](https://www.robopol.sk)

---
+++ youtube_skipper.py
@@ -0,0 +1,89 @@
+import time
+from pynput.mouse import Button, Controller as MouseController
+import keyboard
+from PIL import ImageGrab
+import pyautogui
+
+print("""
+    Script has been initialized
+***************************************************************************************************
+Author: robopol.sk
+The script is useful for automatically skipping ads on YouTube (full screen) using the skip button.
+
+press F6 - start script 
+press F8 - stop script
+
+This script works for a resolution of 3840 x 2160, 2560 x 1440.
+
+*************************************************************************************************** 
+""")   
+
+def is_color_close_to_white(color):
+    # Definujte prah pre blízkosť k bielej farbe
+    threshold = 130
+    # Farba je blízka k bielej, ak je každá zložka (R, G, B) väčšia ako prah
+    return all(value >= threshold for value in color)
+
+def compare_arrays(array1, array2, threshold=0.91):
+    # Počet zhody v poliach
+    matching_count = sum(1 for a, b in zip(array1, array2) if a == b)
+    # Pomer zhody k celkovému počtu prvkov v poliach
+    match_ratio = matching_count / len(array1)    
+    return match_ratio >= threshold
+
+def run_script():
+    # natavenie oblasti pre screen
+    area_width = 26
+    area_height = 22
+    # Získanie rozlíšenia obrazovky
+    screen_width, screen_height = pyautogui.size()
+    global center_x, center_y
+    if screen_width==3840 and screen_height==2160:
+        center_x = screen_width - 51
+        center_y = screen_height - 170
+        saved_binary_values = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    elif screen_width==2560 and screen_height==1440:
+        center_x = screen_width - 37
+        center_y = screen_height - 110
+        saved_binary_values = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    else:
+        # Rozlišenie nie je podporované
+        print(f"This script does not work for the resolution {screen_width} , {screen_height}")
+        time.sleep(4)
+        exit()  # Ukončenie programu po 4 sekundách
+    # Snímok obrazovky v okolí zadanej pozície myši
+    screenshot = ImageGrab.grab(bbox=(center_x - area_width // 2, center_y - area_height // 2, center_x + area_width // 2, center_y + area_height // 2))    
+    pixel_colors = list(screenshot.getdata())    
+    # Uložte snímok obrazovky do súboru
+    screenshot.save("screenshot.png")    
+    # Vytvorte nové pole hodnôt 0 a 1 na základe blízkosti farby k bielej farbe
+    binary_values = [1 if is_color_close_to_white(color) else 0 for color in pixel_colors]        
+    # Porovnanie polí a výpis výsledku
+    if compare_arrays(binary_values, saved_binary_values):        
+        return("true")
+    else:        
+        return("false")
+
+# Premenná, ktorá označuje, či skript má bežať
+script_running = False
+# Vytvorenie inštancie ovládača myši
+mouse_controller = MouseController()
+
+while True:
+    if keyboard.is_pressed('F6') and not script_running:
+        print("Script has been started")
+        script_running = True
+    time.sleep(0.6)    
+    if keyboard.is_pressed('F8'):
+        print("Script has been completed")
+        break    
+    elif keyboard.is_pressed('F6') and not script_running:
+        print("Script has been started")
+        script_running = True    
+    if script_running:
+        category=run_script()
+        if category=="true":
+                pass
+        if keyboard.is_pressed('F8'):
+            print("Script has been completed")
+            break
